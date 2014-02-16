e0b153851a64db2d82b98c50d71b045d74cc27d5
Alex Mitchell <arghargh200@gmail.com>
2014-02-16 21:57:15 +0000

    Added key retrieval and user data persistence
    if you're testing on windows alongside myself make a folder structure
    under your C: drive that looks like this:
    C:\etc\openvpn\keys\

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
+//load user data if it exists, otherwise make it exist
+if(!file_exists("/etc/openvpn/vpndata.json")){file_put_contents("/etc/openvpn/vpndata.json","{}");}
+$vpndata=json_decode(file_get_contents("/etc/openvpn/vpndata.json"),true);
+
 //shows a login page
 function showLogin() {
 	$body='<h1>Login!</h1><hr>
-<form action="?" method="POST">
+<form action="?" method="GET">
 <table>
 <tr>
  <td>Paypal E-Mail:</td>
@@ -21,23 +25,25 @@ function showLogin() {
 </td></tr>
 </table>
 </form>
-<h2>If you are registering, please use the email your paypal account is tied to! If we can\'t tell who sent us money and your paypal email\'s not your registration email, we can\'t give you a VPN key!
+If you are registering, <b>please use the email your PayPal account is tied to!</b> If we can\'t tell who sent us money and your PayPal email\'s not your registration email, we can\'t give you a VPN key!<br><br>
 By registering for and using this service you agree to a few basic rules:
-<ol><li>No illegal operations or hacking! That means any kind of hacking attempts to other sites, our sites, our users on our sites, or anything illegal in the USA, Germany, and the UK. Again, key and email permanently revoked, no exceptions.</li>
-<li>No attempting to sign up for multiple VPN accounts. You can connect multiple devices under one key.
-Don\'t download things over 1GB through the VPN. We pay money to host the server with a set amount of bandwidth; we don\'t need to have the server shut down because you wanted 15GB of text files. Again, permanent key revocation, no exeptions.</li></ol>
-These rules are subject to change at any time, and it is your duty to know! If you get caught with your pants down and don\'t like it, too bad.';
+<ol><li>No illegal operations or hacking! That means any kind of hacking attempts to other sites, our sites, our users on our sites, or <b>anything illegal in the USA, Germany, and the UK.</b> Again, key and email permanently revoked, no exceptions.</li>
+<li>No attempting to sign up for multiple VPN accounts. You can connect multiple devices under one key.</li>
+<li>Don\'t download things over 1GB through the VPN. We pay money to host the server with a set amount of bandwidth; we don\'t need to have the server shut down because you wanted 15GB of text files. Again, permanent key revocation, no exeptions.</li></ol>
+These rules are subject to change at any time, and it is your duty to know! If you get your key revoked and you don\'t like it, that\'s too bad!';
 	return $body;
 }
 
-function doLogin($user,$pass,$reging) {
+function doLogin($username,$password,$reging) {
+	global $vpndata;
+	$token=hash("ripemd160",$username."-".$password);
 	if($reging===false){
 		//check user/pass
-		if ($vpndata['users'][$username]['username']==$username && $vpndata['users'][$username]['password']==$password){
-			$token=hash("ripemd160",$username"-"$password);
+		if ($vpndata['users'][$token]['username']==$username && $vpndata['users'][$token]['password']==$password){
+			$token=hash("ripemd160",$username."-".$password);
 			header("HTTP/1.1 302 Found");
-			if ($vpndata['users'][$username]['authority'] == 0){
-				header("Location: ?token=".$token."&action=");
+			if ($vpndata['users'][$token]['authority'] == 0){
+				header("Location: ?token=".$token."&action=getkey");
 				$body='<h1>Registered! <a href="?action=getkey&token='.$token.'>Get your key!</a></h1>';
 			}else{
 				header("Location: ?token=".$token."&action=admin");
@@ -47,17 +53,18 @@ function doLogin($user,$pass,$reging) {
 			$body='<h1>Login incorrect!</h1>';
 		}
 	}elseif($reging===true){
-		if(!isset($vpndata['users'][$username])){
-			$token=hash("ripemd160",$username"-"$password);
+		if(!isset($vpndata['users'][$token])){
+			$token=hash("ripemd160",$username."-".$password);
 			//store user data into array
-			$vpndata['users'][$username]['username']=$username;  //WHOA GEEZ NO WAY
-			$vpndata['users'][$username]['password']=$password;  //sha256'd, don't worry
-			$vpndata['users'][$username]['regtime']=time(); //for possible expiration of unactivated accounts? idk
-			$vpndata['users'][$username]['status']='1';  //0 is active, 1 is ungenerated key, 2 is revoked/banned.
-			$vpndata['users'][$username]['authority']='0';  //0 is user, 1 is admin
-			$body="<h1>You've been registered! You need to send a payment to us in order for us to activate your account and generate your keys.</h1><br>Questions? <a href="irc://irc.stormbit.net/neo">See us on IRC!</a>"';
+			$vpndata['users'][$token]['username']=$username;  //WHOA GEEZ NO WAY
+			$vpndata['users'][$token]['password']=$password;  //sha256'd, don't worry
+			$vpndata['users'][$token]['regtime']=time(); //for possible expiration of unactivated accounts? idk
+			$vpndata['users'][$token]['status']='1';  //0 is active, 1 is ungenerated key, 2 is revoked/banned.
+			$vpndata['users'][$token]['token']=$token; 
+			$vpndata['users'][$token]['authority']='0';  //0 is user, 1 is admin
+			$body='<h1>You\'ve been registered! You need to send a payment to us in order for us to activate your account and generate your keys.</h1><br>Questions? <a href="irc://irc.stormbit.net/neo">See us on IRC!</a>"';
 		}else{
-			$body='<h1>Registration failed! That email\'s already in use! Please use your Paypal account\'s associated address, otherwise we can\'t give you your VPN key.</h1>'
+			$body='<h1>Registration failed! That email\'s already in use! Please use your Paypal account\'s associated address, otherwise we can\'t give you your VPN key.</h1>';
 			//tell the user the email used to register is in use
 		}
 	}else{
@@ -67,11 +74,13 @@ function doLogin($user,$pass,$reging) {
 }
 
 function showAdmin($token) {
+	global $vpndata;
 
 	return $body;
 }
 
 function doAdmin($task,$token) {
+	global $vpndata;
 
 	return $body;
 }
@@ -156,6 +165,7 @@ if(!isset($_REQUEST["token"])){
 			//sha256 this shit <_<
 			$password=hash("sha256",$_REQUEST["password"]);
 			$username=filter_var($_REQUEST["username"],FILTER_SANITIZE_EMAIL);
+			$token=hash("ripemd160",$username."-".$password);
 			if($_REQUEST["action"]=="register"){
 				//user attempting to register
 				$body=doLogin($username,$password,true);
@@ -163,10 +173,30 @@ if(!isset($_REQUEST["token"])){
 				//user attempting to login
 				$body=doLogin($username,$password,false);
 			}else{$body='<h1>You\'re not logged in! <a href="?">Wanna?</a></h1>';}
-		}else{$body=showLogin();genHtml($body);}
+		}else{$body=showLogin();}
 //	}else{$body='<h1>You\'re not logged in! <a href="?">Wanna?</a></h1>';}
 }elseif(isset($_REQUEST["token"])){
-//actual other junk here
+if(!isset($_REQUEST["action"]) || $_REQUEST["action"] == null ){$body="<h1>No action specified!";}
+	if($_REQUEST["action"]=="getkey"){
+		if($vpndata['users'][$_REQUEST["token"]]['status']=='1'){
+			$body='<h1>Key is not active yet!</h1>';
+		}elseif($vpndata['users'][$_REQUEST["token"]]['status']=='2'){
+			$body='<h1 color="red">Account Banned!</h1>';
+		}else{
+			$doGen=false;
+			$filename="/etc/openvpn/easy-rsa/2.0/keys/".$token.".ovpn";
+			header('Content-Type: application/octet-stream');
+			header("Content-Transfer-Encoding: Binary"); 
+			header("Content-disposition: attachment; filename=\"" . basename($filename) . "\""); 
+			readfile($filename);
+		}
+	}else{
+		$body='<h1>Action not implemented!</h1>';
+	}
+
 }
 
+if($doGen!==false){genHtml($body);}
+file_put_contents("/etc/openvpn/vpndata.json",json_encode($vpndata));
+ 
 ?>
